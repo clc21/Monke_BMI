d11b5d9f997e1590f704efcb5a990ae193705fa0
hnisofia <158060419+hnisofia@users.noreply.github.com>
2025-02-25 17:10:07 +0000

    Update applyPCA.m description :)

--- applyPCA.m
+++ applyPCA.m
@@ -1,4 +1,10 @@
 function [pcaParams, reducedFeatures] = applyPCA(X)
+
+% IMPLEMENTATION : [pcaParams, reducedFeatures] = applyPCA(spikeRates);
+% INPUT: m by n matrix
+% OUTPUT: reducedFeatures --> principal components
+%         pcaParams --> projectionMatrix, explainedVar, numComponents (number of PCA components), mean, cumulative variance
+          
     % Normalize data
     X=X-mean(X,1);
     
@@ -23,7 +29,6 @@ function [pcaParams, reducedFeatures] = applyPCA(X)
     % we have at least 2 components
     numComponents=find(cumVar>=0.95, 1);
     numComponents=max(numComponents,2)
-
     
     % Get projection matrix
     pcaParams.projectionMatrix=eigVectors(:, 1:numComponents);
