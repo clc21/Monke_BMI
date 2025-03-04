11cefee2a7ee99c892d34cb3f613e314e16ac06c
hnisofia <158060419+hnisofia@users.noreply.github.com>
2025-03-04 15:05:35 +0000

    Update applyLDA.m

--- applyLDA.m
+++ applyLDA.m
@@ -4,15 +4,19 @@ function [ldaModel] = applyLDA(X, y)
   % y: class labels
   
   % Get unique classes
-  classes = unique(y);
-  numClasses = length(classes);
-  [numSamples, numFeatures] = size(X);
+  classes=unique(y);
+  numClasses=length(classes);
+  [numSamples, numFeatures]=size(X);
   
   % Compute class means
   classMeans = zeros(numClasses, numFeatures);
-  for i = 1:numClasses
-      classMeans(i, :) = mean(X(y == classes(i), :), 1);
-  end
+  classSizes = zeros(numClasses, 1);
+  
+    for i = 1:numClasses
+        classIdx = (y == classes(i));
+        classMeans(i, :) = mean(X(classIdx, :), 1);
+        classSizes(i) = sum(classIdx);
+    end
   
   % Compute global mean
   globalMean = mean(X, 1);
@@ -20,31 +24,41 @@ function [ldaModel] = applyLDA(X, y)
   % Compute between-class scatter matrix
   Sb = zeros(numFeatures, numFeatures);
   for i = 1:numClasses
-      n_i = sum(y == classes(i));
       meanDiff = classMeans(i, :) - globalMean;
-      Sb = Sb + n_i * (meanDiff' * meanDiff);
+      Sb = Sb + classSizes(i) * (meanDiff' * meanDiff);
   end
+
   
   % Compute within-class scatter matrix
   Sw = zeros(numFeatures, numFeatures);
   for i = 1:numClasses
       classIdx = (y == classes(i));
       classData = X(classIdx, :);
-      classMeanMatrix = repmat(classMeans(i, :), sum(classIdx), 1);
-      Sw = Sw + (classData - classMeanMatrix)' * (classData - classMeanMatrix);
+      classMeanMatrix = repmat(classMeans(i, :), classSizes(i), 1);
+      deviation = classData - classMeanMatrix;
+      Sw = Sw + deviation' * deviation;
   end
   
-  % Compute the transformation matrix by solving Sb*w = lambda*Sw*w
-  % This is equivalent to finding eigenvectors of inv(Sw)*Sb
-  % Note: Using pinv for potential numerical stability with high-dim data
-  [V, D] = eig(pinv(Sw) * Sb);
+  % Apply regularization to within-class scatter matrix
+  lambda = 0.001; % Regularization parameter
+  Sw_reg = Sw + lambda * trace(Sw) * eye(numFeatures);
+  
+  % Compute the transformation matrix by solving the generalized eigenvalue problem
+  [V, D] = eig(Sb, Sw_reg);
   
   % Sort eigenvectors by eigenvalues in descending order
-  [~, sortIdx] = sort(diag(D), 'descend');
+  [eigValues, sortIdx] = sort(diag(D), 'descend');
   V = V(:, sortIdx);
   
   % Keep only the eigenvectors for n_classes - 1 dimensions
-  V = V(:, 1:min(numClasses-1, numFeatures));
+  numComponents = min(numClasses-1, numFeatures);
+  V = V(:, 1:numComponents);
+  
+  % Normalize the eigenvectors to make projections more comparable to PCA
+  % This ensures the scale of projections is reasonable
+  for i = 1:size(V, 2)
+      V(:, i) = V(:, i) / norm(V(:, i));
+  end
   
   % Project class means to LDA space
   projectedMeans = classMeans * V;
@@ -55,7 +69,5 @@ function [ldaModel] = applyLDA(X, y)
   ldaModel.projectedMeans = projectedMeans; % Class means in LDA space
   ldaModel.classes = classes;          % Class labels
   ldaModel.globalMean = globalMean;    % Global mean
-
-  % Project the data onto LDA space
-  % X_lda = (X - ldaModel.globalMean) * ldaModel.V;
+  ldaModel.eigenvalues = eigValues(1:numComponents); % Eigenvalues for components
 end
