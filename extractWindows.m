653e31b266525e5ce96380ddcd63cef10dac840b
Trevi03 <147492833+Trevi03@users.noreply.github.com>
2025-03-13 14:17:51 +0000

    add extractWindows.m

---
+++ extractWindows.m
@@ -0,0 +1,65 @@
+function [spikeRate,handKinematics,time_bins] = extractWindows(trial,trialNumber,angle,args)
+%extractWindows Extract the spike rate, hand Pos&Vel&Acc in time windows
+%   trial
+arguments
+    trial struct
+    trialNumber double
+    angle double
+    args.isStruct logical = true
+    args.winSz double = 20  % ms
+    args.winStp double = 20
+end
+
+if args.isStruct
+    spike_train = trial(trialNumber,angle).spikes;
+    pos_array = trial(trialNumber,angle).handPos;
+else
+    spike_train = trial.spikes;
+    pos_array = trial.handPos;
+end
+
+dt = 1e-3;                            % sampling period
+n_neurons = size(spike_train,1);      % number of neurons
+n_pos = size(pos_array,1);            % number of directions
+n_time = size(spike_train,2);
+time_range = 1:size(spike_train,2);  % slice of interest
+n_wind = floor((n_time - args.winSz) / args.winStp) + 1;
+
+% velocity is derivative of displacement
+pos_prev = zeros(n_pos,n_time);
+pos_prev(:,1) = pos_array(:,1);
+pos_prev(:,2:end) = pos_array(:,1:end-1);
+vel_array = (pos_array-pos_prev)/dt;
+
+% acceleration is derivative of velocity\
+vel_prev = zeros(n_pos,n_time);
+vel_prev(:,1) = vel_array(:,1);
+vel_prev(:,2:end) = vel_array(:,1:end-1);
+acc_array = (vel_array-vel_prev)/dt;
+
+% initialise arrays
+spikeRate = zeros(n_neurons, n_wind);
+handPos = zeros(n_pos,n_wind);      % average displacement per window
+handVel = zeros(n_pos,n_wind);      % average velocity per window
+handAcc = zeros(n_pos,n_wind);      % average acceleration per window
+time_bins = zeros(1, n_wind);
+
+% Sliding window loop
+for w = 1:n_wind
+    % Define window range (within 300:600)
+    t_start = (w-1) * args.winStp + 1;
+    t_end = t_start + args.winSz - 1;
+
+    % Compute spike rate and kinematics
+    spikeRate(:, w) = sum(spike_train(:, t_start:t_end), 2)/ args.winSz * 1000; % Convert to Hz
+    handPos(:, w) = mean(pos_array(:, t_start:t_end),2);
+    handVel(:, w) = mean(vel_array(:, t_start:t_end),2);
+    handAcc(:, w) = mean(acc_array(:, t_start:t_end),2);
+
+    % Store time bin center (relative to the time range)
+    time_bins(w) = time_range(round((t_start + t_end) / 2));
+end
+
+handKinematics = cat(1,handPos(1:2,:),handVel(1:2,:),handAcc(1:2,:));
+
+end
